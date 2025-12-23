8ea9d9539b19ce144eabcb3a35918db3b8bdfdbc
Nurlan1995 <61499167+Nurlan1995@users.noreply.github.com>
2025-12-23 13:34:35 +0400

    test3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React immediately
+          Learn React immediately !!!!!!!!!!!!!!!!
         </a>
       </header>
     </div>
